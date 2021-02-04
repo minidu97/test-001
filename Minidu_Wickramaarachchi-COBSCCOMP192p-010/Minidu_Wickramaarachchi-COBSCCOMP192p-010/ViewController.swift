e4b12981da8240a03d66363673cf8344927449f4
minidu97 <42093242+minidu97@users.noreply.github.com>
2021-02-04 13:59:21 +0530

    First Commit

---
+++ Minidu_Wickramaarachchi-COBSCCOMP192p-010/Minidu_Wickramaarachchi-COBSCCOMP192p-010/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minidu_Wickramaarachchi-COBSCCOMP192p-010
+//
+//  Created by Minidu Wickramaarachchi on 2/3/21.
+//  Copyright © 2021 Minidu Wickramaarachchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
